31931d5a0f98603bd719717a6ccf8f5999a39e17
FerrerMark <ferrerjohnmark26@gmail.com>
2025-03-19 21:20:49 +0800

    fix logo

--- login.php
+++ login.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="./css/login.css">
 </head>
 <body style="">
-    <h1>HELLO WORLD</h1>
     <div class="login-container">
         <div class="form-container">
             <div class="logo">
